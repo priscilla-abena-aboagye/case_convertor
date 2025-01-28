7ad250555f9581e37ab476d61d812b6306702250
Abena <priscillaabenaaboagye@gmail.com>
2025-01-28 17:55:34 +0000

    the duplicated file for the list comprehension

---
+++ duplicate.py
@@ -0,0 +1,18 @@
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = []
+    return "".join(snake_cased_char_list).strip("_")
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    print(convert_to_snake_case('aLongAndComplexString'))
+main()
