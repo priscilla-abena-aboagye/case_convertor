cbfa02610f4fa5f646100e1e439d0f646f0bb968
Abena <priscillaabenaaboagye@gmail.com>
2025-01-28 17:43:42 +0000

    the main function

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
 
 
 def main():
-    convert_to_snake_case("aLongAndComplexString")
-    print(convert_to_snake_case)
+    print(convert_to_snake_case('aLongAndComplexString'))
+main()
